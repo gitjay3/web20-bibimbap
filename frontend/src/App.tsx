85f3c187598d9cf6a5e76955de307108892b049e
siyoung <wfs0502@gmail.com>
2025-12-18 14:53:12 +0900

    fix: 머지 과정에서 발생한 오류 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,6 @@
-import { ReservationList } from './features/reservation-list';
-import { CreateEventPage } from './features/create-event/ui/CreateEventPage';
-import './features/reservation-list/styles.css';
-
-function App() {
-  // TODO: 라우팅 추가 필요 - 임시로 ReservationList만 렌더링
-  return <ReservationList />;
 import { Toaster } from "sonner";
 import { CreateEventPage } from "./features/create-event/ui/CreateEventPage";
 import ReservationPage from "./features/reservation/ui/ReservationPage";
-
 function App() {
   return (
     <>
@@ -18,5 +10,4 @@ function App() {
     </>
   );
 }
-
 export default App;
