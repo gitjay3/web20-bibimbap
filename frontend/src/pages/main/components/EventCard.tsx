bd4f6032fef76868e57396962ce1abc83caadf4e
한지은 <hjn510450@gmail.com>
2026-01-12 16:35:10 +0900

    feat: 이벤트 예약 단위 라벨 ApplicationUnitLabel 컴포넌트 구현

--- frontend/src/pages/main/components/EventCard.tsx
+++ frontend/src/pages/main/components/EventCard.tsx
@@ -1,20 +1,32 @@
+import ApplicationUnitLabel from '@/components/ApplicationUnitLabel';
 import EventCategoryLabel from '@/components/EventCategoryLabel';
 import EventStatusLabel from '@/components/EventStatusLabel';
 import type { Event } from '@/types/event';
 import formatKoreanDateTime from '@/utils/formatKoreanDateTime';
+import CalendarIcon from '@/assets/icons/calendar-clock.svg?react';
 
-function EventCard({ category, status, title, description, startAt, endAt }: Event) {
+interface EventCardProps {
+  event: Event;
+}
+
+function EventCard({ event }: EventCardProps) {
+  const { category, status, title, description, startAt, endAt, applicationUnit } = event;
+
   return (
-    <div className="border-neutral-border-default hover:border-brand-border-default flex h-62.5 w-full flex-col justify-between rounded-xl border p-5 transition hover:shadow-md">
+    <div className="border-neutral-border-default hover:border-brand-border-default flex h-62.5 w-full cursor-pointer flex-col justify-between rounded-xl border p-5 transition hover:shadow-md">
       <div className="flex flex-col gap-3">
         <div className="flex gap-2">
+          <ApplicationUnitLabel applicationUnit={applicationUnit} />
           <EventCategoryLabel category={category} />
           <EventStatusLabel status={status} />
         </div>
         <div className="text-20 font-extrabold">{title}</div>
         <div className="text-12 text-neutral-text-secondary">{description}</div>
       </div>
-      <div className="bg-neutral-surface-default text-12 text-neutral-text-secondary flex h-8 items-center gap-2 rounded-md px-3">{`${formatKoreanDateTime(startAt)} ~ ${formatKoreanDateTime(endAt)}`}</div>
+      <div className="bg-neutral-surface-default text-12 text-neutral-text-secondary flex h-8 items-center gap-2 rounded-md px-3">
+        <CalendarIcon className="text-brand-text-primary h-4 w-4" />
+        {`${formatKoreanDateTime(startAt)} ~ ${formatKoreanDateTime(endAt)}`}
+      </div>
     </div>
   );
 }
