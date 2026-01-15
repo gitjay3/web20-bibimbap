a4e6d5c60290fbef2433ec18b1c6484642205e00
한지은 <hjn510450@gmail.com>
2026-01-15 13:47:29 +0900

    feat: GET 'auth/me'를 요청하는 getMe 함수 구현

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,7 @@
+import type { User } from '@/types/user';
+import api from './api';
+
+export const getMe = async (): Promise<User> => {
+  const response = await api.get<User>('/auth/me');
+  return response.data;
+};
